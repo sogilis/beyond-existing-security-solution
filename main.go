6dbaa243dd7727db0ebb647dd8fff97b51d76a18
Willy Malvault <willy@sogilis.com>
2022-12-30 15:15:57 +0100

    WIP readme and smal refacto

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/jackc/pgx/v5"
 )
 
 const HTTP_LISTENNING_ADDR = "0.0.0.0:8080"
@@ -34,24 +31,4 @@ func main() {
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hey from BESS server !")
-}
-
-func connectDB() {
-	// urlExample := "postgres://username:password@localhost:5432/database_name"
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer conn.Close(context.Background())
-
-	// var name string
-	// var weight int64
-	// err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-	// 	os.Exit(1)
-	// }
-
-	// fmt.Println(name, weight)
 }
