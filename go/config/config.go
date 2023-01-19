5854763567759b21771ad0804d220ceba181ae4e
Willy Malvault <willy@sogilis.com>
2023-01-19 17:36:55 +0100

    fixing vault token config

--- go/config/config.go
+++ go/config/config.go
@@ -8,7 +8,7 @@ func Load() {
 	viper.AutomaticEnv()
 	viper.SetDefault("DB_HOST", "localhost")
 	viper.SetDefault("HTTP_LISTENNING_ADDR", "0.0.0.0:8080")
-	viper.SetDefault("BESS_VAULT_ADDR", "localhost")
+	viper.SetDefault("BESS_VAULT_ADDR", "http://localhost:8200/")
 	viper.SetDefault("BESS_VAULT_TOKEN_PATH", "./tokens/bess")
 }
 
